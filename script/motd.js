780bdd0801a57c6e7894d0c7685c341613935bb1
Dex <82985536+BinaryDex@users.noreply.github.com>
2023-11-12 22:57:05 -0500

    port vscode

---
+++ script/motd.js
@@ -0,0 +1,36 @@
+let message = {
+    messages: [
+        'haiii :3 ', 
+        'irisdex@proton.me       ', 
+        'dex wuz here ', 
+        'ruining the internet since 2019 ', 
+        'wsg   ', 
+        'forever unhittable ',
+        'grrrrrrrrr ', 
+        'irisware > all ', 
+        'femboys >>> ', 
+        'rip script-ware ', 
+        'rip v3rmillion ', 
+        'one day or day one? '
+    ],
+    up: true,
+    index: 0,
+    count: 0,
+    start: function() {
+        message.interval = setInterval(() => {
+            if (message.count == message.messages[message.index].length) {
+                message.up = false
+                clearInterval(message.interval)
+                setTimeout(message.start, 750)
+            } else if (message.count == 1) {
+                message.up = true
+                message.index = message.index == message.messages.length -1 ? 0 : ++message.index
+            }
+
+            message.count = message.up ? ++message.count : --message.count
+            document.getElementById('index_message').textContent = message.messages[message.index].slice(0, message.count)
+        }, 75);
+    }
+}
+
+message.start()
